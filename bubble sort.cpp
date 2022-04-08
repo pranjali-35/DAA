cd678fe6688e7fe95a89c7326e0487163c133431
pranjali-35 <91174464+pranjali-35@users.noreply.github.com>
2022-04-08 12:38:07 +0530

    Update bubble sort.cpp

--- bubble sort.cpp
+++ bubble sort.cpp
@@ -4,17 +4,9 @@
     3. Number of times swapping is done*/
 
 #include <iostream>
-
 using namespace std;
 
-int main()
-{
-    int n;
-    cin >> n;
-    int *arr = new int(n);
-    for(int i = 0 ; i < n ; i++)
-        cin >> arr[i];
-    
+void BubbleSort(int n, int *arr){
     int compare_count = 0;
     int swap_count = 0;
     for(int i = 0 ; i < n-1 ; i++){
@@ -28,9 +20,18 @@ int main()
     for(int i = 0 ; i < n ; i++)
         cout << arr[i] << " ";
     cout << endl;
-    
     cout << "Number of times comparision is done : " << compare_count << endl;
     cout << "Number of times swapping operation is done : " << swap_count << endl;
+}
 
+int main()
+{
+    int n;
+    cin >> n;
+    int *arr = new int(n);
+    for(int i = 0 ; i < n ; i++)
+        cin >> arr[i];
+    BubbleSort(n,arr);
+    
     return 0;
 }
